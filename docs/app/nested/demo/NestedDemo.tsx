2bf72e0ea84ea02dba0403aecf60ec0f423f4b1d
Janpot <2109932+Janpot@users.noreply.github.com>
2025-07-21 19:08:13 +0200

    improve demo

--- docs/app/nested/demo/NestedDemo.tsx
+++ docs/app/nested/demo/NestedDemo.tsx
@@ -3,31 +3,35 @@ import { Panel, Resizer } from "react-flex-panels";
 export default function NestedDemo() {
   return (
     <Panel group direction="row">
-      <Panel initialSize="200px" className="demo-panel">
+      <Panel initialSize="200px" className="demo-panel panel-left">
         Left Panel
         <small>Fixed width</small>
       </Panel>
       <Resizer />
       <Panel group direction="column" minSize="100px">
-        <Panel initialSize="150px" className="demo-panel">
+        <Panel
+          initialSize="150px"
+          minSize="50px"
+          className="demo-panel panel-middle-top"
+        >
           Middle Top
           <small>Fixed height</small>
         </Panel>
         <Resizer />
-        <Panel group direction="row">
-          <Panel className="demo-panel">
+        <Panel group direction="row" minSize="50px">
+          <Panel minSize="20px" className="demo-panel panel-bottom-left">
             Bottom Left
             <small>Flexible</small>
           </Panel>
           <Resizer />
-          <Panel className="demo-panel">
+          <Panel minSize="20px" className="demo-panel panel-bottom-right">
             Bottom Right
             <small>Flexible</small>
           </Panel>
         </Panel>
       </Panel>
       <Resizer />
-      <Panel initialSize="180px" className="demo-panel">
+      <Panel initialSize="180px" className="demo-panel panel-right">
         Right Panel
         <small>Fixed width</small>
       </Panel>
