e0738d376cfe852be385a7f7cf44ac02f5083cba
Janpot <2109932+Janpot@users.noreply.github.com>
2025-07-27 20:27:56 +0200

    clean it up

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+};
+
+export default config;
