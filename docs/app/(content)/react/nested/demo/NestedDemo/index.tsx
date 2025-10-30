83f96e63e0940eea2278ada3b079203e024fed23
Janpot <2109932+Janpot@users.noreply.github.com>
2025-10-30 23:45:50 +0100

    Update index.tsx

--- docs/app/(content)/react/nested/demo/NestedDemo/index.tsx
+++ docs/app/(content)/react/nested/demo/NestedDemo/index.tsx
@@ -4,7 +4,10 @@ import styles from './index.module.css';
 export default function NestedDemo() {
   return (
     <Panel group orientation="horizontal">
-      <Panel initialSize="25%" className={`${styles.demoPanel} ${styles.panelLeft}`}>
+      <Panel
+        initialSize="25%"
+        className={`${styles.demoPanel} ${styles.panelLeft}`}
+      >
         Left Panel
         <small>Fixed width</small>
       </Panel>
@@ -20,19 +23,28 @@ export default function NestedDemo() {
         </Panel>
         <Resizer />
         <Panel group orientation="horizontal" minSize="15%">
-          <Panel minSize="10%" className={`${styles.demoPanel} ${styles.panelBottomLeft}`}>
+          <Panel
+            minSize="10%"
+            className={`${styles.demoPanel} ${styles.panelBottomLeft}`}
+          >
             Bottom Left
             <small>Flexible</small>
           </Panel>
           <Resizer />
-          <Panel minSize="10%" className={`${styles.demoPanel} ${styles.panelBottomRight}`}>
+          <Panel
+            minSize="10%"
+            className={`${styles.demoPanel} ${styles.panelBottomRight}`}
+          >
             Bottom Right
             <small>Flexible</small>
           </Panel>
         </Panel>
       </Panel>
       <Resizer />
-      <Panel initialSize="22%" className={`${styles.demoPanel} ${styles.panelRight}`}>
+      <Panel
+        initialSize="22%"
+        className={`${styles.demoPanel} ${styles.panelRight}`}
+      >
         Right Panel
         <small>Fixed width</small>
       </Panel>
